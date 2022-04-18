03f924f4668538008d10af20abfb99b025e29938
Felipe Leal <felipe.leal@compasso.com.br>
2022-04-17 23:33:16 -0300

    Component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Flex } from "@chakra-ui/react";
+import {Logo} from './Header/Logo';
+import { SearchBox } from './Header/SearchBox';
+import {Profile} from "../components/Profile/Profile"
+
+export function Header() {
+  return (
+    <Flex 
+      as="header"
+      width="100%"
+      maxW={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      <Logo />
+
+      <SearchBox />
+      
+      <Profile />
+     
+    </Flex>
+  )
+}
